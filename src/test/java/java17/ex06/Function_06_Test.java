e72da6b50c3e4ff567b738843fe44526756bf1db
malauzet <30903872+malauzet@users.noreply.github.com>
2026-07-23 11:21:26 +0200

    tp3 terminé, tous les tests passent

--- src/test/java/java17/ex06/Function_06_Test.java
+++ src/test/java/java17/ex06/Function_06_Test.java
@@ -18,7 +18,8 @@ public class Function_06_Test {
     // TODO la méthode retourne une chaîne de caractères de la forme [age=<AGE>] (exemple : [age=12])
     String formatAge(Supplier<Person> supplier) {
         // TODO
-        return null;
+        int ageSup = supplier.get().getAge();
+        return "[age=" + ageSup + "]";
     }
     // end::formatAge[]
 
@@ -26,7 +27,7 @@ public class Function_06_Test {
     @Test
     public void test_supplier_formatAge() throws Exception {
         // TODO compléter le test unitaire pour qu'il soit passant
-        String result = formatAge(null);
+        String result = formatAge(() -> new Person("Bob", "Le Bricoleur", 35, "AliceBob"));
 
         assert result.equals("[age=35]");
     }
