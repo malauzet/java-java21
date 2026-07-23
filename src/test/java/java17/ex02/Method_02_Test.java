d350848d139467861b924e97492c7d2de7905931
malauzet <30903872+malauzet@users.noreply.github.com>
2026-07-23 09:53:44 +0200

    tp terminé, tous les tests passent

--- src/test/java/java17/ex02/Method_02_Test.java
+++ src/test/java/java17/ex02/Method_02_Test.java
@@ -19,6 +19,10 @@ public class Method_02_Test {
         // TODO créer une méthode String format()
         // TODO la méthode retourne une chaîne de la forme [<nb_personnes> persons]
         // TODO exemple de résultat : "[14 persons]", "[30 persons]"
+
+        default String format() {
+            return "[" + findAll().size() + " persons]";
+        }
     }
     // end::IDao[]
 
@@ -37,6 +41,10 @@ public class Method_02_Test {
         // TODO exemple de résultat : "DaoA[14 persons]", "DaoA[30 persons]"
         // TODO l'implémentation réutilise la méthode format() de l'interface
 
+        @Override
+        public String format() {
+            return "DaoA" + IDao.super.format();
+        }
     }
     // end::DaoA[]
 
@@ -47,6 +55,8 @@ public class Method_02_Test {
 
         // TODO invoquer la méthode format() pour que le test soit passant
         String result = null;
+
+        result = daoA.format();
 
         assert "DaoA[20 persons]".equals(result);
     }
